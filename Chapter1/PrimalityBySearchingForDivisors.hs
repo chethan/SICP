e662bb38a99ceb97181fc00f92a4b79ab9547775
chethan <yourschethu@gmail.com>
2011-03-26 00:24:22 +0530

    Improving the algorithm to find prime numbers

--- Chapter1/PrimalityBySearchingForDivisors.hs
+++ Chapter1/PrimalityBySearchingForDivisors.hs
@@ -1,7 +1,16 @@
-smallest_divisor n = find_divisor n 2
-					 where find_divisor n divisor | (truncate (sqrt (fromIntegral n))) < divisor = n
-						    					  | (rem n divisor)==0 = divisor
-												  | otherwise =  find_divisor n (divisor+1)
+smallest_divisor n next  = find_divisor n 2 next
+					 		where find_divisor n divisor next | (truncate (sqrt (fromIntegral n))) < divisor = n
+						    					  		      | (rem n divisor)==0 = divisor
+												  	          | otherwise =  find_divisor n (next divisor) next
+get_next 2 = 3
+get_next x = x+2
+
+is_prime n = n == smallest_divisor n (1+)
+
+is_fast_prime n = n == smallest_divisor n get_next
+						
+						
+
+
 												
-is_prime n = n == smallest_divisor n												
 												
