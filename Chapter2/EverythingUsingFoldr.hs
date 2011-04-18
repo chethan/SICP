b7becdb02cbb5e4b8102cfde72fa442b4ab917fb
Chethan <yourschethu@gmail.com>
2011-04-19 00:48:26 +0530

    implemented reverse using foldr

--- Chapter2/EverythingUsingFoldr.hs
+++ Chapter2/EverythingUsingFoldr.hs
@@ -2,3 +2,4 @@ map_using_fold f xs =  foldr (\x acc -> (f x):acc) [] xs
 append_using_fold xs ys =  foldr (\x acc -> x:acc) ys xs
 length_using_fold xs = foldr (\x acc -> 1+acc) 0 xs
 hornets_rule xval coeff = foldr (\x acc -> x +(acc * xval)) 0 coeff
+reverse_using_foldr xs = foldr (\x acc ->  acc ++ [x]) [] xs
