80e9b60ae7ef359b3fe532cb8519631e737333da
Chethan Prakash <yourschethu@gmail.com>
2011-04-23 23:57:32 +0530

    More code using Flatmap

--- Chapter2/FlatMap.hs
+++ Chapter2/FlatMap.hs
@@ -5,4 +5,11 @@ flatmap func seq = foldr (\x acc -> x ++ acc) [] (map func seq)
 generate_seq n = flatmap (\x -> map (\y -> (y,x)) [1..(x-1)])[1..(n-1)]
 
 my_permutations (x:[]) = [[x]]
-my_permutations (xs) =  flatmap (\x-> (map (\y -> x:y) (permutations (delete x xs)))) (xs)
+my_permutations (xs) =  flatmap (\x-> (map (\y -> x:y) (my_permutations (delete x xs)))) (xs)
+
+prime_sum_pairs n = let is_prime n = (rem ((n-1) ^ n) n) == (n-1)  
+					in filter (\(a,b,c) -> is_prime c)(flatmap (\x -> map (\y -> (x,y,x+y)) [1..(x-1)])[1..(n)])
+
+prime_sum_pairs_comprehensions n = let is_prime n = (rem ((n-1) ^ n) n) == (n-1)  
+								   in [(i,j,i+j)| i <- [1..n], j <- [1..(i-1)] , is_prime (i+j)] 
+
